1fb64adbc352e3d1017b5c1a4b7ad336e19b6d77
Mary Benitez <meebenitez@gmail.com>
2017-10-25 19:51:25 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
+
+#require 'rack-flash'
 class ApplicationController < Sinatra::Base
 
   set :views, Proc.new { File.join(root, "../views/") }
   register Sinatra::Twitter::Bootstrap::Assets
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+    #use Rack::Flash
+  end
+
+  get '/' do
+    erb :index
+  end
 end
