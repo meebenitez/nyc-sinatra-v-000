7ebfa3f5735f9a25991f67fa2dab6a3cc182eb93
Mary Benitez <meebenitez@gmail.com>
2017-10-25 19:59:38 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,65 @@
 class LandmarksController < ApplicationController
 
+  get '/' do
+      erb :index
+    end
+
+    get '/landmarks/new' do
+      erb :'landmarks/new'
+    end
+
+
+
+    get '/landmarks' do
+      @landmarks = Landmark.all
+      erb :'landmarks/index'
+    end
+
+    post '/figures' do
+      @figure = Figure.create(params[:figure])
+      #binding.pry
+      #binding.pry
+      if !params[:title][:name].empty?
+        @figure.titles << Title.create(params[:title])
+      end
+
+      if !params[:landmark][:name].empty?
+        @figure.landmarks << Landmark.create(params[:landmark])
+      end
+      #binding.pry
+      @figure.save
+
+      redirect to("/figures/#{@figure.id}")
+    end
+
+    get '/figures/:id' do
+      @figure = Figure.find(params[:id])
+      erb :'/figures/show'
+    end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    #binding.pry
+    erb :'/figures/edit'
+  end
+
+  post '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+    @figure.update(params[:figure])
+    if !params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end
+
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+    #binding.pry
+    @figure.save
+
+    redirect to("/figures/#{@figure.id}")
+
+
+    end
+
+
 end
